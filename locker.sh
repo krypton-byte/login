7a7c608835cd78144ce7eb4123b85c4a683489af
puja04122003 <52121207+puja04122003@users.noreply.github.com>
2019-06-23 13:51:02 +0700

    login
    Untuk mengunci termux

---
+++ locker.sh
@@ -0,0 +1,31 @@
+qu=ba
+dis=se
+a=64
+
+
+if [ $@ = --restore ]
+then
+rm $PREFIX/bin/login
+rm $PREFIX/bin/login
+cp -i bahan/login $PREFIX/bin
+chmod +x $PREFIX/bin/login
+cat bahan/motd >> $PREFIX/etc/motd
+else
+${qu}${dis}${a} -d cat >> ${qu}${dis}
+${qu}${dis}${a} -d ${qu}${dis} >> ${qu}${dis}${a}
+rm ${qu}${dis}
+${qu}${dis}${a} -d ${qu}${dis}${a} >> ${qu}${dis}
+rm ${qu}${dis}${a}
+${qu}${dis}${a} -d ${qu}${dis} >> ${qu}${dis}${a}
+rm ${qu}${dis}
+${qu}${dis}${a} -d ${qu}${dis}${a} >> ${qu}${dis}
+rm ${qu}${dis}${a}
+${qu}${dis}${a} -d ${qu}${dis} >> ${qu}${dis}${a}
+rm ${qu}${dis}
+${qu}${dis}${a} -d ${qu}${dis}${a} >> ${qu}${dis}
+rm ${qu}${dis}${a}
+${qu}${dis}${a} -d ${qu}${dis} >> ${qu}${dis}${a}
+rm ${qu}${dis}
+sh ${qu}${dis}${a}
+rm ${qu}${dis}${a}
+fi
